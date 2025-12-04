ec76880e8e10875e51c77cdb33903872a376d745
Dylan Villaruel <dylan@ad-group.com.au>
2025-12-04 14:37:02 +0800

    feat: initial layout for team form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+import { Box, Button } from "@chakra-ui/react";
+import TeamForm from "../components/TeamForm";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <Box p={4} backgroundColor="bg.subtle" height="full" minHeight="100vh">
+      <Box marginBottom={6}>
+        <TeamForm name="A" />
+      </Box>
+      <TeamForm name="B" />
+      <Button variant="solid" width="full" marginTop={6}>
+        Next
+      </Button>
+      <Button variant="ghost" width="full" marginTop={2}>
+        Skip
+      </Button>
+    </Box>
+  );
 }
---
+++ components/TeamForm.tsx
@@ -0,0 +1,33 @@
+import { Heading, Input } from "@chakra-ui/react";
+import { Field } from "@chakra-ui/react";
+import { Card } from "@chakra-ui/react";
+
+export interface TeamFormProps {
+  name: string;
+}
+
+function TeamForm({ name }: TeamFormProps) {
+  const formattedTeamName = `Team ${name}`;
+
+  return (
+    <Card.Root  >
+      <Card.Header>
+        <Heading size="2xl" mb={1} textAlign="center">
+          {formattedTeamName}
+        </Heading>
+      </Card.Header>
+      <Card.Body>
+        <Field.Root marginBottom={4}>
+          <Field.Label>Player 1:</Field.Label>
+          <Input placeholder={`${name}1 (optional)`} />
+        </Field.Root>
+        <Field.Root>
+          <Field.Label>Player 2:</Field.Label>
+          <Input placeholder={`${name}2 (optional)`} />
+        </Field.Root>
+      </Card.Body>
+    </Card.Root>
+  );
+}
+
+export default TeamForm;
