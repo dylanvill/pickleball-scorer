a340900bd56057376ec81660e60e7ff9a19e84fb
Dylan Villaruel <dylan@ad-group.com.au>
2025-12-09 15:43:42 +0800

    feat: added replace to match when match is active

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import OnboardingTabs, {
   OnboardingTabsProps,
 } from "../components/OnboardingTabs";
 import PlayersTabContent from "../components/OnboardingTabs/PlayersTabContent";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FirstServerTabContent from "../components/OnboardingTabs/FirstServerTabContent";
 import useMatch from "../store/useMatch";
 import { TeamKey } from "../store/useTeam/types/Team";
@@ -14,13 +14,19 @@ type ActiveTab = OnboardingTabsProps["activeTab"];
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState<ActiveTab>("players");
-  const { startMatch } = useMatch();
+  const { startMatch, match } = useMatch();
   const router = useRouter();
 
   const handleStartGameClicked = (servingTeam: TeamKey) => {
     startMatch(servingTeam);
     router.replace("/match");
   };
+
+  useEffect(() => {
+    if (match.isActive) {
+      router.replace("/match");
+    }
+  }, [match.isActive, router]);
 
   return (
     <OnboardingTabs
